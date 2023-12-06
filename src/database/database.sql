ff172adb4693633e9f4ca3b025abc612f52e64fe
Yuzo <yuzokamoto@gmail.com>
2023-12-06 19:24:50 -0300

    Update database.sql

--- src/database/database.sql
+++ src/database/database.sql
@@ -11,10 +11,8 @@ CREATE TABLE users (
 INSERT INTO users (id, name, email, password, role)
 VALUES
   -- conta NORMAL e senha = "fulano123"
-	('u001', 'Fulano', 'fulano@email.com', '$2a$12$kUtElOebs1Zl6CBAUc6Ndeit6M/heGgohjgYS.g6c72sbT/y.TVYK', 'NORMAL'),
-	
+  ('u001', 'Fulano', 'fulano@email.com', '$2a$12$kUtElOebs1Zl6CBAUc6Ndeit6M/heGgohjgYS.g6c72sbT/y.TVYK', 'NORMAL'),
   -- conta NORMAL e senha = "beltrana00"
   ('u002', 'Beltrana', 'beltrana@email.com', '$2a$12$gO/aPYDibF3LW/X4cK2vlOHGJi/oAMFvCgU8sRe5W23vZM4oWC.Qy', 'NORMAL'),
-
   -- conta ADMIN e senha = "astrodev99"
-	('u003', 'Astrodev', 'astrodev@email.com', '$2a$12$N86uqg4FewXtEUKNaS.yduFpkL/KS8r1iCq2/heVGOhasXaLWG7ga', 'ADMIN');
+  ('u003', 'Astrodev', 'astrodev@email.com', '$2a$12$N86uqg4FewXtEUKNaS.yduFpkL/KS8r1iCq2/heVGOhasXaLWG7ga', 'ADMIN');
